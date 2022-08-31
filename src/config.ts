0eb3cf944b5dbbca84fdd4ab2f52402a5ed70375
Nicole Analise Cox <nicoleanalisecox@gmail.com>
2022-08-31 18:45:44 +0100

    configure some global TW styles and add favicon

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Nicole Analise Cox | Multi-passionate technical creative';
+export const SITE_DESCRIPTION = 'Nicole is: Writing | Coding | Working | Being 🌱 Sharing my thoughts and experiences navigating adulthood, a career in tech and my special interests.';
---
+++ src/styles/styles.ts
@@ -0,0 +1,10 @@
+// COLOURS
+export const typographyPrimary = 'text-slate-900'
+export const typographySecondary = 'text-slate-500'
+export const typographyPrimaryDark = 'text-white'
+export const typographySecondaryDark = 'text-slate-400'
+export const typographyAccent = 'text-violet-600'
+
+export const backgroundPrimary = 'bg-white'
+
+export const UIAccent = 'bg-violet-600'
