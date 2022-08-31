86daca399e988d14deba2360abe48574d35c2639
Nicole Analise Cox <nicoleanalisecox@gmail.com>
2022-08-31 19:26:51 +0100

    customise footer

---
+++ src/components/FooterContent.tsx
@@ -0,0 +1,65 @@
+import { backgroundPrimary, hoverTypographyAccent, iconSize6, typographySecondary } from '../styles/styles'
+
+const today = new Date();
+
+const navigation = [
+    {
+        name: 'Polywork',
+        href: 'https://www.polywork.com/nicoleanalisecox',
+        icon: (props: any) => (
+            <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+                <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M19.125 0H4.875A4.865 4.865 0 0 0 0 4.875v14.25C0 21.825 2.175 24 4.875 24h6.6c2.7 0 4.875-2.175 4.875-4.875V16.65h2.775c2.7 0 4.875-2.175 4.875-4.875v-6.9C24 2.175 21.825 0 19.125 0zM16.5 1.275h2.625a3.6 3.6 0 0 1 3.6 3.6v2.7H16.5v-6.3zM15.075 9v6.45H8.85V9h6.225zM8.85 1.2h6.225v6.375H8.85V1.2zM1.275 4.8a3.6 3.6 0 0 1 3.6-3.6H7.5v6.375H1.275V4.8zM7.5 9v6.45H1.2V9h6.3zm0 13.725H4.8a3.6 3.6 0 0 1-3.6-3.6V16.8h6.3v5.925zm7.575-3.525a3.6 3.6 0 0 1-3.6 3.6H8.85v-5.925h6.225V19.2zm7.65-7.35a3.6 3.6 0 0 1-3.6 3.6H16.5V9h6.225v2.85z" />
+            </svg>
+        ),
+    },
+    {
+        name: 'GitHub',
+        href: 'https://github.com/nicoleanalisecox',
+        icon: (props: any) => (
+            <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+                <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+            </svg>
+        ),
+    },
+    {
+        name: 'LinkedIn',
+        href: 'https://www.linkedin.com/in/nicoleanalisecox/',
+        icon: (props: any) => (
+            <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+                <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+            </svg>
+        ),
+    },
+    {
+        name: 'Twitter',
+        href: 'https://twitter.com/nicoleisxyz',
+        icon: (props: any) => (
+            <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+                <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
+            </svg>
+        ),
+    }
+]
+
+const Footer = () => (
+    <footer className={backgroundPrimary}>
+        <div className="max-w-7xl mx-auto pt-6 pb-20 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8 lg:py-4">
+            <div className="flex justify-center space-x-6 md:order-2">
+                {navigation.map((item) => (
+                    <a key={item.name} href={item.href} target="_blank" className={`${typographySecondary} ${hoverTypographyAccent}`}>
+                        <span className="sr-only">{item.name}</span>
+                        <item.icon className={iconSize6} aria-label={item.name} />
+                    </a>
+                ))}
+            </div>
+            <div className="mt-8 md:mt-0 md:order-1">
+                <p className={`text-center text-xs ${typographySecondary}`}>&copy; {today.getFullYear()} nicole analise cox</p>
+            </div>
+        </div>
+    </footer>
+)
+
+export default Footer
--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -3,8 +3,12 @@ export const typographyPrimary = 'text-slate-900'
 export const typographySecondary = 'text-slate-500'
 export const typographyPrimaryDark = 'text-white'
 export const typographySecondaryDark = 'text-slate-400'
-export const typographyAccent = 'text-violet-600'
+export const typographyAccent = 'text-indigo-600'
+export const hoverTypographyAccent = `hover:${typographyAccent}`
 
 export const backgroundPrimary = 'bg-white'
 
-export const UIAccent = 'bg-violet-600'
+export const UIAccent = 'bg-indigo-600'
+
+// SIZING
+export const iconSize6 = 'h6 w-6'
