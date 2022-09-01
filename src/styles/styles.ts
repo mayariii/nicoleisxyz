3eed2d0b5383ba294583ede570c8bc8008b7e8bb
Nicole Analise Cox <nicoleanalisecox@gmail.com>
2022-09-01 13:23:45 +0100

    migrate homepage index file

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -2,18 +2,26 @@
 export const typographyPrimary = 'text-slate-900'
 export const typographySecondary = 'text-slate-500'
 export const typographyPrimaryInverted = 'text-white'
-export const typographyPrimaryDark = 'text-white'
-export const typographySecondaryDark = 'text-slate-400'
+export const typographyPrimaryDark = 'dark:text-white'
+export const typographySecondaryDark = 'dark:text-slate-400'
 export const typographyAccent = 'text-indigo-600'
 export const hoverTypographyAccent = 'hover:text-indigo-600'
 
 export const backgroundPrimary = 'bg-white'
+export const backgroundPrimaryDark = 'dark:bg-slate-900'
+export const backgroundPrimaryHover = 'hover:bg-slate-50'
 
 export const UIAccent = 'bg-indigo-600'
 export const hoverUIAccent = 'hover:bg-indigo-700'
 
+export const strokeAccent = 'stroke-indigo-600'
+
+export const borderDefault = 'border-gray-300'
+
 // SIZING
-export const iconSize6 = 'h6 w-6'
+export const iconSize4 = 'h-4 w-4'
+export const iconSize6 = 'h-6 w-6'
+
 
 // LINKS
 export const linkBase = `block rounded-md py-2 px-3 text-base font-medium no-underline`
