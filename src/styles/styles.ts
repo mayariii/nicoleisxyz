7bf224b0bf93c49011f8771334d7f0ad2502a548
Nicole Analise Cox <nicoleanalisecox@gmail.com>
2022-09-01 15:57:27 +0100

    add support for light/dark mode toggle

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,27 +1,24 @@
 // COLOURS
-export const typographyPrimary = 'text-slate-900'
-export const typographySecondary = 'text-slate-500'
+export const typographyPrimary = 'text-slate-900 dark:text-white'
+export const typographySecondary = 'text-slate-500 dark:text-slate-400'
 export const typographyPrimaryInverted = 'text-white'
-export const typographyPrimaryDark = 'dark:text-white'
-export const typographySecondaryDark = 'dark:text-slate-400'
-export const typographyAccent = 'text-indigo-600'
-export const hoverTypographyAccent = 'hover:text-indigo-600'
+export const typographyAccent = 'text-indigo-600 dark:text-indigo-500'
+export const hoverTypographyAccent = 'hover:text-indigo-600 dark:hover:text-indigo-500'
 
-export const backgroundPrimary = 'bg-white'
+export const backgroundPrimary = 'bg-white dark:bg-slate-900'
 export const backgroundPrimaryDark = 'dark:bg-slate-900'
-export const backgroundPrimaryHover = 'hover:bg-slate-50'
+export const backgroundPrimaryHover = 'hover:bg-slate-50 dark:hover:bg-slate-800'
 
-export const UIAccent = 'bg-indigo-600'
-export const hoverUIAccent = 'hover:bg-indigo-700'
+export const UIAccent = 'bg-indigo-600 dark:bg-indigo-500'
+export const hoverUIAccent = 'hover:bg-indigo-700 dark:hover:bg-indigo-600'
 
-export const strokeAccent = 'stroke-indigo-600'
+export const strokeAccent = 'stroke-indigo-600 dark:stroke-indigo-400'
 
-export const borderDefault = 'border-gray-300'
+export const borderDefault = 'border-slate-300 dark:border-slate-50'
 
 // SIZING
 export const iconSize4 = 'h-4 w-4'
 export const iconSize6 = 'h-6 w-6'
-
 
 // LINKS
 export const linkBase = `block rounded-md py-2 px-3 text-base font-medium no-underline`
