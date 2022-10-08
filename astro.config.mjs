1faddf62e4d357be6d9710f99a4245f4da8c7c81
Nicole Analise Cox <nicoleanalisecox@gmail.com>
2022-10-08 18:02:59 +0100

    add robots.txt integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from 'astro-robots-txt';
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), tailwind(), image()]
+  site: 'https:nicoleis.xyz',
+  integrations: [mdx(), sitemap(), react(), tailwind(), image(), robotsTxt()]
 });
